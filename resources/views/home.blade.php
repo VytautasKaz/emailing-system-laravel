110658298b94b8a1e3a01d44a8160bad90562070
VytautasKaz <vytautas.kazakevicius2@gmail.com>
2021-05-09 14:19:32 +0300

    home view update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,9 @@
         <div class="row justify-content-center">
             <div style="text-align: center" class="col-md-8">
                 <h1>Welcome!</h1><br>
-                <p><strong>Log in to access app's features!</strong></p>
+                @if (Auth::guest())
+                    <p><strong>Log in to access app's features!</strong></p>
+                @endif
             </div>
         </div>
     </div>
