e79b03e12c05d2a75bf7be0ab7344106adeaa934
VytautasKaz <vytautas.kazakevicius2@gmail.com>
2021-05-09 14:00:52 +0300

    welcome page update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div style="text-align: center" class="col-md-8">
-                <h1>Welcome!</h1>
+                <h1>Welcome!</h1><br>
+                <p><strong>Log in to access app's features!</strong></p>
             </div>
         </div>
     </div>
